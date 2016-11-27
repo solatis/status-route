b8079b96e86ccc8ffb0502d62ba957ab6bd556c2
Leon Mergen <leon@solatis.com>
2016-11-27 11:36:54 +0100

    Fixes 80 char line length

--- src/status_route/yada_adapter.clj
+++ src/status_route/yada_adapter.clj
@@ -7,5 +7,7 @@
   [result]
   (yada/resource
    {:description "Service status"
-    :produces #{"application/edn;q=0.9" "application/json;q=0.8" "application/transit+json;q=0.7"}
+    :produces #{"application/edn;q=0.9"
+                "application/json;q=0.8"
+                "application/transit+json;q=0.7"}
     :methods {:get {:response #(status result)}}}))
